700f7a9010ee9d0854171412e449efc82e1af5f2
Valeria <valeria.rimbu@vilt-group.com>
2019-09-23 16:48:22 +0200

    proyectos

---
+++ proj1/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.linkB').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('.nav__bottom').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.nav__bottom').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
