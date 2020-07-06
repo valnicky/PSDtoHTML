92dbb6654dbafeab65b9308e89eeefd5129ad5f8
Valeria <valery0007@gmail.com>
2020-07-06 13:42:56 +0200

    js

--- proj2/js/script.js
+++ proj2/js/script.js
@@ -5,11 +5,27 @@ $(document).ready(init);
 
 
 function init() {
-    const selMenu = document.querySelector('.header2 .menu');// console.log(selMenu);
+    const selMenu = document.querySelector('.header2 .menu'); // console.log(selMenu);
     const ul = document.querySelector('.header2 .list2');
     selMenu.click(function () {
         ul.setAttribute('display', 'flex');
         // $('.list2').toggle();
         console.log('click' + ul);
     });
+
+    toggleFunc();
+}
+
+function toggleFunc() {
+    const menubf = document.querySelector('.menu');
+    menubf.addEventListener('click', () => {
+        const list2 = document.querySelector('.list2');
+        if (list2.style.display === 'none';) {
+            list2.style.display = 'block';
+        }
+
+    });
+
+
+
 }
